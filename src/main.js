6025d163ad6769b8f025e55a839c57d69aaa4f12
Gary Kaganas <gary.kaganas@britecore.com>
2019-07-16 08:49:31 -0400

    Add the design system

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import BcDesignSystem from '@britecore/bc-design-system'
+
+Vue.use(BcDesignSystem)
+
 Vue.config.productionTip = false
 
 new Vue({
